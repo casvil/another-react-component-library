120e9c1b9e33b3d7a6c4ba4e2922a2c204e9d3ee
David Casanellas <davidcasanellas@dorna.com>
2021-03-27 02:48:57 +0100

    Fix hover and add prop types

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ disabled, kind, onClick, size, style, value }) => (
   <input
     className={`
       active:bg-${kind}-400 
-      hover:bg-${disabled ? `disabled` : kind}-700 
+      ${disabled ? `` : `hover:bg-${kind}-700`} 
       bg-${kind}-500 
       focus:outline-none 
       px-3 
@@ -20,5 +20,15 @@ const Button = ({ disabled, kind, onClick, size, style, value }) => (
     disabled={disabled}
   />
 );
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  kind: PropTypes.oneOf(["default", "primary", "success", "warning", "danger"]),
+};
+
+Button.defaultProps = {
+  disabled: false,
+  kind: "default",
+};
 
 export default Button;
