88044e2dab46e1e0f781c786ad055c45a3dbddd7
David Casanellas <davidcasanellas@dorna.com>
2021-03-27 02:21:49 +0100

    Add active, hover, focus, bg-opacity and cursor classes

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,18 @@ import React from "react";
 
 const Button = ({ disabled, kind, onClick, size, style, value }) => (
   <input
-    className={`bg-${kind}-500 text-${kind} px-3 py-2`}
+    className={`
+      active:bg-${kind}-400 
+      hover:bg-${disabled ? `disabled` : kind}-700 
+      bg-${kind}-500 
+      focus:outline-none 
+      px-3 
+      py-2 
+      rounded
+      text-${kind} 
+      ${disabled ? `bg-opacity-75` : `bg-opacity-100`}
+      ${disabled ? `cursor-not-allowed` : `cursor-pointer`}
+    `}
     type="button"
     value={value}
     onClick={onClick}
