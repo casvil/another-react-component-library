9de5531e36868355f3a581864bd6dbdffcb71b3e
David Casanellas <davidcasanell@gmail.com>
2025-06-15 17:55:02 +0300

    Export components

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,8 @@
-import { Button } from "./stories/Button";
+import { Button } from "./Button/Button";
+import { ErrorMessage } from "./ErrorMessage/ErrorMessage";
+import { HelperText } from "./HelperText/HelperText";
+import { Input } from "./Input/Input";
+import { InputField } from "./InputField/InputField";
+import { Label } from "./Label/Label";
 
-export { Button } 
+export { Button, ErrorMessage, HelperText, Input, InputField, Label };
