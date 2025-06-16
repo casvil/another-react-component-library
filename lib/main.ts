4c369d5d7b6ad182862cc9a535f5f94346d502d1
David Casanellas <davidcasanell@gmail.com>
2025-06-16 07:59:15 +0300

    Export components

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,19 @@
 import { Button } from "./Button/Button";
 import { ErrorMessage } from "./ErrorMessage/ErrorMessage";
 import { HelperText } from "./HelperText/HelperText";
+import { IconButton } from "./IconButton/IconButton";
 import { Input } from "./Input/Input";
 import { InputField } from "./InputField/InputField";
 import { Label } from "./Label/Label";
+import { Tooltip } from "./Tooltip/Tooltip";
 
-export { Button, ErrorMessage, HelperText, Input, InputField, Label };
+export {
+  Button,
+  ErrorMessage,
+  HelperText,
+  IconButton,
+  Input,
+  InputField,
+  Label,
+  Tooltip,
+};
