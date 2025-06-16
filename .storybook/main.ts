f310a7c14d012c9d274362f64301a01e911f5f8c
David Casanellas <davidcasanell@gmail.com>
2025-06-16 20:54:18 +0300

    Set config.base if production and simplify build-storybook script

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,22 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../lib/**/*.mdx",
-    "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../lib/**/*.mdx", "../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/another-react-component-library/";
+    }
+    return config;
+  },
 };
 export default config;
