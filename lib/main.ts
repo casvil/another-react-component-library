2fae8fec5880ab6aa804dc7fd999fbb4745ae40f
David Casanellas <davidcasanell@gmail.com>
2025-06-16 16:42:45 +0300

    Export components

--- lib/main.ts
+++ lib/main.ts
@@ -1,19 +1,23 @@
 import { Button } from "./Button/Button";
+import { Clock } from "./Clock/Clock";
 import { ErrorMessage } from "./ErrorMessage/ErrorMessage";
 import { HelperText } from "./HelperText/HelperText";
 import { IconButton } from "./IconButton/IconButton";
 import { Input } from "./Input/Input";
 import { InputField } from "./InputField/InputField";
 import { Label } from "./Label/Label";
+import { TimeDisplay } from "./TimeDisplay/TimeDisplay";
 import { Tooltip } from "./Tooltip/Tooltip";
 
 export {
   Button,
+  Clock,
   ErrorMessage,
   HelperText,
   IconButton,
   Input,
   InputField,
   Label,
+  TimeDisplay,
   Tooltip,
 };
