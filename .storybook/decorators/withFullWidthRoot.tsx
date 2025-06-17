d84111533c3e60356ef6098ee3eefee7eca4150f
David Casanellas <davidcasanell@gmail.com>
2025-06-17 17:02:40 +0300

    Add withFullWidthRoot decorator

---
+++ .storybook/decorators/withFullWidthRoot.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import type { Decorator } from "@storybook/react";
+
+/**
+ * Sets `width: 100%` on the #storybook-root element for a specific story.
+ */
+export const withFullWidthRoot: Decorator = (Story) => {
+  useEffect(() => {
+    const root = document.getElementById("storybook-root");
+
+    if (root) root.style.width = "100%";
+
+    return () => {
+      if (root) root.style.width = "";
+    };
+  }, []);
+
+  return <Story />;
+};
