b571c37ac98fffd408377aae9a4dd44a0e47a831
David Casanellas <davidcasanell@gmail.com>
2025-06-17 15:42:36 +0300

    Add radio component

---
+++ lib/Radio/Radio.tsx
@@ -0,0 +1,51 @@
+import { forwardRef, InputHTMLAttributes, useId } from "react";
+import clsx from "clsx";
+
+import { Label } from "../Label/Label";
+
+export interface RadioProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "type"> {
+  label?: string;
+  id?: string;
+  className?: string; // applies to <input>
+  wrapperClassName?: string; // applies to outer div
+  labelClassName?: string; // aplies to Label
+  "aria-describedby"?: string;
+}
+
+/**
+ * Radio atom component.
+ * Accessible, stylized radio input with optional label using the custom Label component.
+ */
+export const Radio = forwardRef<HTMLInputElement, RadioProps>(
+  (
+    { id, label, className, labelClassName, wrapperClassName, ...props },
+    ref
+  ) => {
+    const radioId = id || useId();
+
+    const radioElement = (
+      <input
+        id={radioId}
+        type="radio"
+        ref={ref}
+        className={clsx(
+          "h-4 w-4 text-indigo-600 focus:ring-2 focus:ring-indigo-500 border-gray-300 rounded-full",
+          className
+        )}
+        {...props}
+      />
+    );
+
+    if (!label) return radioElement;
+
+    return (
+      <div className={clsx("flex items-center space-x-2", wrapperClassName)}>
+        {radioElement}
+        <Label className={labelClassName} htmlFor={radioId}>
+          {label}
+        </Label>
+      </div>
+    );
+  }
+);
