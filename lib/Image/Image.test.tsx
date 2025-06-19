8f25c71e4341de83ec0feb1af1c525b7874c3f54
David Casanellas <davidcasanell@gmail.com>
2025-06-19 17:00:22 +0300

    Lint errors

--- lib/Image/Image.test.tsx
+++ lib/Image/Image.test.tsx
@@ -1,5 +1,5 @@
 import { createRef } from 'react';
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import { Image } from './Image';
