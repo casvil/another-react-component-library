f6ac939067adeb879e67802a1e435fa1d2417701
David Casanellas <davidcasanell@gmail.com>
2025-06-21 00:08:07 +0300

    Remove unnecessary virtual path in tests and set import jest-dom efectively in vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import { fileURLToPath } from 'url';
+import '@testing-library/jest-dom';
 
 const CI = process.env.CI === 'true';
 
@@ -9,12 +9,5 @@ export default defineConfig({
     globals: true,
     setupFiles: './vitest.setup.ts',
     reporters: CI ? [['default', { summary: false }]] : ['verbose'],
-  },
-  resolve: {
-    alias: {
-      'lucide-react/icons': fileURLToPath(
-        new URL('./node_modules/lucide-react/dist/esm/icons', import.meta.url),
-      ),
-    },
   },
 });
