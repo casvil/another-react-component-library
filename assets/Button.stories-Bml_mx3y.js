675686164295c9c86432b3b963c0eaeb3c25a215
casvil <casvil@users.noreply.github.com>
2025-06-29 17:15:38 +0000

    deploy: 75eb3c029dba57a98adadcba806ee83f3ad2298c

---
+++ assets/Button.stories-Bml_mx3y.js
@@ -0,0 +1,58 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{B as c}from"./Button-Dd8ugMuc.js";import{c as N}from"./createLucideIcon-DFpa-8el.js";import"./iframe-ESOGW_Ls.js";import"./clsx-B-dksMZM.js";import"./classes-BD7lNhBs.js";/**
+ * @license lucide-react v0.515.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const _=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],I=N("arrow-left",_);/**
+ * @license lucide-react v0.515.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const R=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],T=N("arrow-right",R),H={title:"atoms/Button",component:c,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{control:"select",options:["primary","secondary","tertiary"]},size:{control:"select",options:["sm","md","lg"]},iconPosition:{control:"radio",options:["left","right"]}}},e={args:{label:"Primary Button",variant:"primary",size:"md"}},a={args:{label:"Secondary Button",variant:"secondary",size:"md"}},s={args:{label:"Tertiary Button",variant:"tertiary",size:"md"}},t={args:{label:"Next Step",variant:"primary",size:"md",icon:T,iconPosition:"right"}},o={args:{label:"Back",variant:"secondary",size:"md",icon:I,iconPosition:"left"}},n={args:{label:"Disabled",variant:"primary",size:"md",disabled:!0}},i={render:()=>r.jsxs("div",{className:"space-x-2",children:[r.jsx(c,{label:"Small",size:"sm"}),r.jsx(c,{label:"Medium",size:"md"}),r.jsx(c,{label:"Large",size:"lg"})]})};var m,l,d;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Primary Button',
+    variant: 'primary',
+    size: 'md'
+  }
+}`,...(d=(l=e.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var p,u,y;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    label: 'Secondary Button',
+    variant: 'secondary',
+    size: 'md'
+  }
+}`,...(y=(u=a.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var g,b,z;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    label: 'Tertiary Button',
+    variant: 'tertiary',
+    size: 'md'
+  }
+}`,...(z=(b=s.parameters)==null?void 0:b.docs)==null?void 0:z.source}}};var h,S,v;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: 'Next Step',
+    variant: 'primary',
+    size: 'md',
+    icon: ArrowRight,
+    iconPosition: 'right'
+  }
+}`,...(v=(S=t.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var x,B,f;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    label: 'Back',
+    variant: 'secondary',
+    size: 'md',
+    icon: ArrowLeft,
+    iconPosition: 'left'
+  }
+}`,...(f=(B=o.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};var P,j,L;n.parameters={...n.parameters,docs:{...(P=n.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled',
+    variant: 'primary',
+    size: 'md',
+    disabled: true
+  }
+}`,...(L=(j=n.parameters)==null?void 0:j.docs)==null?void 0:L.source}}};var k,w,A;i.parameters={...i.parameters,docs:{...(k=i.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: () => <div className="space-x-2">
+      <Button label="Small" size="sm" />
+      <Button label="Medium" size="md" />
+      <Button label="Large" size="lg" />
+    </div>
+} satisfies StoryObj<ButtonProps>`,...(A=(w=i.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};const $=["Primary","Secondary","Tertiary","WithRightIcon","WithLeftIcon","Disabled","AllSizes"];export{i as AllSizes,n as Disabled,e as Primary,a as Secondary,s as Tertiary,o as WithLeftIcon,t as WithRightIcon,$ as __namedExportsOrder,H as default};
